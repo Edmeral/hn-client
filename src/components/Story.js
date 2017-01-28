2828ccc29f081727787ea4678cd6f80302f1dff5
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2017-01-28 20:47:07 +0000

    add pretty formatting for time inside Story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import formatTime from '../helpers/formatTime'
 
-const Story = (props) => (
-	<li>
-	  <a href="{props.story.url}">{props.story.title}</a>
-	  {props.story.time} by {props.story.by}
+const Story = (props) => {
+	let now = Math.floor(Date.now() / 1000)
+	return <li>
+	  <a href="{props.story.url}">{props.story.title}</a>{' '}
+		{formatTime(now - props.story.time)} by {props.story.by}
 	</li>
-)
+}
+	
+
+
 
 export default Story
